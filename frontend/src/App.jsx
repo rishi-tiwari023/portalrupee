e27238b4abe8b75946c8b067995ff404f70da19f
Rishi Tiwari <umesh896073@gmail.com>
2025-11-13 22:48:19 +0530

    Wire RBIGuideline and Footer into app layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar';
+import RBIGuideline from './components/RBIGuideline';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <RBIGuideline />
+      <Footer />
     </div>
   );
 }
