98447330202b9f3ef78e7455f5ed542a4c32b8e6
Gagan Gupta <2k23.cscys2310911@gmail.com>
2026-04-09 21:08:46 +0530

    add user and account models

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'First name is required'],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Last name is required'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      required: [true, 'Mobile number is required'],
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    role: {
+      type: String,
+      enum: ['CUSTOMER', 'CASHIER', 'MANAGER'],
+      default: 'CUSTOMER',
+    },
+    // tpin to be added on Day 9
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
